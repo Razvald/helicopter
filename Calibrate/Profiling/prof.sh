b968f7d33e2606501d3a3f566874e8c7854e4192
Razvald <pdepar@yandex.ru>
2025-02-17 14:44:23 +0700

    change nsys

--- Calibrate/Profiling/prof.sh
+++ Calibrate/Profiling/prof.sh
@@ -20,29 +20,32 @@ if [ ! -f "$SCRIPT_PATH" ]; then
     exit 1
 fi
 
-# Создание папки для отчётов, если её нет
-if [ ! -d "$REPORT_DIR" ]; then
-    mkdir -p "$REPORT_DIR"
-    echo "Создана папка для отчётов: $REPORT_DIR"
-fi
-
 # Запуск профилирования
 echo "Профилирование файла: $SCRIPT_PATH"
-nsys profile -t nvtx,osrt --force-overwrite=true --duration=10 --trace-fork-before-exec=true --stats=true --output=report_temp python3 "$SCRIPT_PATH"
+nsys profile -t nvtx,osrt --force-overwrite=true --trace-fork-before-exec=true --stats=true --output=report_temp python3 "$SCRIPT_PATH"
 
 # Поиск последнего созданного файла с расширением .nsys-rep
 LATEST_REPORT=$(find . -type f -name "report_temp*.nsys-rep" -printf "%T@ %p\n" | sort -n | tail -1 | cut -d' ' -f2-)
 
-# Проверка, найден ли файл
+# Поиск последнего созданного файла с расширением .sqlite
+LATEST_REPORT_SQL=$(find . -type f -name "report_temp*.sqlite" -printf "%T@ %p\n" | sort -n | tail -1 | cut -d' ' -f2-)
+
+# Удаление файла .sqlite, если он существует
+if [ -n "$LATEST_REPORT_SQL" ]; then
+    echo "Удаление временного файла: $LATEST_REPORT_SQL"
+    rm "$LATEST_REPORT_SQL"
+fi
+
+# Проверка, найден ли файл .nsys-rep
 if [ -n "$LATEST_REPORT" ]; then
     echo "Последний созданный отчет: $LATEST_REPORT"
 
-    # Создание нового имени для отчёта на основе текущей даты и времени
+    # Создание нового имени для отчета на основе текущей даты и времени
     TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
     NEW_REPORT_NAME="report_$TIMESTAMP.nsys-rep"
     NEW_REPORT_PATH="$REPORT_DIR/$NEW_REPORT_NAME"
 
-    # Перемещение отчёта с новым именем
+    # Перемещение отчета с новым именем
     mv "$LATEST_REPORT" "$NEW_REPORT_PATH"
 
     echo "Отчет перемещен в: $NEW_REPORT_PATH"
