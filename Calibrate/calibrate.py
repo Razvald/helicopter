59f79debfc6aac79a64c9148ea4fc0f82b36fe68
Razvald <pdepar@yandex.ru>
2025-03-04 12:14:09 +0700

    clean

--- Calibrate/calibrate.py
+++ Calibrate/calibrate.py
@@ -1,29 +1,25 @@
 # %%
-import nvtx
+import matplotlib.pyplot as plt
+import os
+import json
+import cv2
+import numpy as np
+from time import time
 
-with nvtx.annotate("Init: Imports", color="dodgerblue"):
-    import matplotlib.pyplot as plt
-    import os
-    import json
-    import cv2
-    import numpy as np
-    from time import time
-
-    import vio_ort as vio_ort
-    import vio_ortO as vio_ort_original
-    import vio_ort_ai_copy as vio_ort_ai
+import vio_ort as vio_ort
+import vio_ortO as vio_ort_original
+import vio_ort_ai_copy as vio_ort_ai
 # %%
 # Инициализация глобальных параметров
-with nvtx.annotate("Init: Global VIO & Params", color="dodgerblue"):
-    odometry = vio_ort.VIO(lat0=54.889668, lon0=83.1258973333, alt0=0)
-    odometry_org = vio_ort_original.VIO(lat0=54.889668, lon0=83.1258973333, alt0=0)
-    odometry_ai = vio_ort_ai.VIO(lat0=54.889668, lon0=83.1258973333, alt0=0)
-    set_dir = '2024_12_15_15_31_8_num_3'
-    json_files = sorted([f for f in os.listdir(set_dir) if f.endswith('.json')])
-    start = 1000
-    count_json = 100
-    lat_VIO, lon_VIO, alt_VIO = [], [], []
-    lat_GPS, lon_GPS, alt_GPS = [], [], []
+odometry = vio_ort.VIO(lat0=54.889668, lon0=83.1258973333, alt0=0)
+odometry_org = vio_ort_original.VIO(lat0=54.889668, lon0=83.1258973333, alt0=0)
+odometry_ai = vio_ort_ai.VIO(lat0=54.889668, lon0=83.1258973333, alt0=0)
+set_dir = '2024_12_15_15_31_8_num_3'
+json_files = sorted([f for f in os.listdir(set_dir) if f.endswith('.json')])
+start = 1000
+count_json = 100
+lat_VIO, lon_VIO, alt_VIO = [], [], []
+lat_GPS, lon_GPS, alt_GPS = [], [], []
 
 # %%
 def run_vio(odometry, json_files, start, count_json):
@@ -56,20 +52,17 @@ def run_vio(odometry, json_files, start, count_json):
         'alt_GPS': alt_GPS,
     }
 # %%
-with nvtx.annotate("Main: Execute opt", color="black"):
-    timer = time()
-    results_optimized = run_vio(odometry, json_files, start, count_json)
-    print(f"Execution time for opt: {time() - timer:.2f} seconds")
+timer = time()
+results_optimized = run_vio(odometry, json_files, start, count_json)
+print(f"Execution time for opt: {time() - timer:.2f} seconds")
 
-with nvtx.annotate("Main: Execute org", color="black"):
-    timer = time()
-    results_original = run_vio(odometry_org, json_files, start, count_json)
-    print(f"Execution time for org: {time() - timer:.2f} seconds")
-    
-with nvtx.annotate("Main: Execute ai", color="black"):
-    timer = time()
-    results_ai = run_vio(odometry_ai, json_files, start, count_json)
-    print(f"Execution time for ai: {time() - timer:.2f} seconds")
+timer = time()
+results_original = run_vio(odometry_org, json_files, start, count_json)
+print(f"Execution time for org: {time() - timer:.2f} seconds")
+
+timer = time()
+results_ai = run_vio(odometry_ai, json_files, start, count_json)
+print(f"Execution time for ai: {time() - timer:.2f} seconds")
 # %%
 def calculate_errors(results):
     lat_diff = np.array(results['lat_VIO']) - np.array(results['lat_GPS'])
@@ -86,10 +79,9 @@ def calculate_errors(results):
         'alt_rmse': alt_rmse
     }
 # %%
-with nvtx.annotate("Main: Calculate errors", color="black"):
-    errors_optimized = calculate_errors(results_optimized)
-    errors_original = calculate_errors(results_original)
-    errors_ai = calculate_errors(results_ai)
+errors_optimized = calculate_errors(results_optimized)
+errors_original = calculate_errors(results_original)
+errors_ai = calculate_errors(results_ai)
 # %%
 def print_errors(errors, label):
     print(f"Errors for {label}:")
@@ -97,10 +89,9 @@ def print_errors(errors, label):
     print(f"  Longitude RMSE: {errors['lon_rmse']:.10f}")
     print(f"  Altitude RMSE: {errors['alt_rmse']:.10f}")
 # %%
-with nvtx.annotate("Print Errors", color="darkgreen"):
-    """print_errors(errors_optimized, "Optimized VIO")
-    print_errors(errors_original, "Original VIO")
-    print_errors(errors_ai, "AI VIO")"""
+print_errors(errors_optimized, "Optimized VIO")
+print_errors(errors_original, "Original VIO")
+print_errors(errors_ai, "AI VIO")
 # %%
 # Функция для построения графика с GPS и VIO
 def plot_comparison(results_optimized, results_original):
@@ -154,13 +145,9 @@ def plot_comparison(results_optimized, results_original):
 
     plt.tight_layout()
     plt.show()
+
 # %%
-results_optimized['lat_VIO'] = [lat * 1000 for lat in results_optimized['lat_VIO']]
-results_original['lat_VIO'] = [lat * 1000 for lat in results_original['lat_VIO']]
-results_ai['lat_VIO'] = [lat * 1000 for lat in results_ai['lat_VIO']]
-# %%
-with nvtx.annotate("Plot Comparison", color="purple"):
-    plot_comparison(results_optimized, results_original)
+plot_comparison(results_optimized, results_original)
 
 # %%
 print(len(results_optimized['lat_VIO']))
