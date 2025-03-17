93e08eb8df77c4a3ee1a69d2a32d773980b23c0c
Razvald <pdepar@yandex.ru>
2025-03-17 15:03:03 +0700

    save

--- Calibrate/test.py
+++ Calibrate/test.py
@@ -3,39 +3,41 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Чтение данных из файла CSV
-df = pd.read_csv('vio_results_updated.csv')
+df = pd.read_csv('Debugs/vio_results_updated.csv')
 
 # Приведение столбцов
 df['Max Iters'] = df['Max Iters'].fillna('None')  # Преобразование NaN в 'None'
 
-plt.figure(figsize=(12, 6))
+plt.figure(figsize=(6, 6))
 
 # График 1: Зависимость RMSE от Detection Threshold
-plt.subplot(1, 3, 1)
 sns.boxplot(data=df, x='Detection Threshold', y='RMSE', hue='Top_k', palette='viridis')
 plt.title('Влияние Detection Threshold на RMSE при различных Top_k')
 plt.xlabel('Detection Threshold')
 plt.ylabel('RMSE')
 plt.legend(title='Top_k')
+plt.tight_layout()
+plt.savefig('Debugs/graph1_detection_threshold_vs_rmse.png')
 
 # График 2: Влияние Trace Depth
-plt.subplot(1, 3, 2)
 sns.barplot(data=df, x='Trace depth', y='RMSE', hue='Rotation method', ci=None, palette='magma')
 plt.title('Влияние Trace Depth на RMSE (по методам поворота)')
 plt.xlabel('Trace Depth')
 plt.ylabel('RMSE')
 plt.legend(title='Rotation Method')
+plt.tight_layout()
+plt.savefig('Debugs/graph2_trace_depth_vs_rmse.png')
 
 # График 3: Влияние Max Iters
-plt.subplot(1, 3, 3)
 sns.lineplot(data=df[df['Max Iters'] != 'None'], x='Max Iters', y='RMSE', hue='Detection Threshold', style='Top_k', markers=True, palette='coolwarm')
 plt.title('Влияние Max Iters на RMSE (по Detection Threshold)')
 plt.xlabel('Max Iters')
 plt.ylabel('RMSE')
 plt.legend(title='Detection Threshold / Top_k')
-
 plt.tight_layout()
-#plt.show()
+plt.savefig('Debugs/graph3_max_iters_vs_rmse.png')
+
+plt.show()
 
 
 # Поиск минимального значения RMSE
